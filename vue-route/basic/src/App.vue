cc09d387672f08390b2efc9ca79cb712d7c377d4
mu-mo <2205567926@qq.com>
2018-08-08 11:44:42 +0800

    add vue-route

---
+++ vue-route/basic/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1> Hello Vue</h1>
+    <p>
+      <router-link to="/foo">Go to Foo</router-link>
+      <router-link to="/bar">Go to Bar</router-link>
+    </p>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  watch: {
+    $route(to, from) {
+      console.log(this.$route);
+    }
+  }
+};
+</script>
+
+<style>
+.router-link-active {
+  color: red;
+}
+</style>
---
+++ vue-route/basic/src/routes.js
@@ -0,0 +1,14 @@
+import Bar from "./Bar.vue";
+import Foo from "./Foo.vue";
+
+const routes = [{
+    path: "/foo",
+    component: Foo
+  },
+  {
+    path: "/bar",
+    component: Bar
+  }
+];
+
+export default routes
